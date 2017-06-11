fb57f346024d1790698a0185a824c979160a31dd
lucdelrio <lucasgdelrio93@gmail.com>
2017-06-11 19:40:29 -0300

    Borra clase

--- app/src/main/java/com/app/faltauno/activities/MainActivity.java
+++ app/src/main/java/com/app/faltauno/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package com.app.faltauno.activities;
 
 import android.content.Intent;
-import android.content.pm.LabeledIntent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -13,28 +12,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.app.faltauno.R;
-import com.app.faltauno.request.BusProvider;
 import com.app.faltauno.request.Communicator;
-import com.app.faltauno.request.ErrorEvent;
-import com.app.faltauno.request.MatchEvent;
-import com.app.faltauno.response.MatchData;
 import com.app.faltauno.response.MatchDataResponse;
-import com.app.faltauno.response.MatchResults;
 import com.app.faltauno.services.ApiService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
-import static android.support.design.R.styleable.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/app/faltauno/services/ApiService.java
+++ app/src/main/java/com/app/faltauno/services/ApiService.java
@@ -5,26 +5,19 @@ package com.app.faltauno.services;
  */
 import com.app.faltauno.response.MatchData;
 import com.app.faltauno.response.MatchDataResponse;
-import com.app.faltauno.response.MatchResults;
 
-import java.util.Date;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface ApiService {
 
     @POST("matches")
     Call <Void> postMatch (@Body MatchData match);
 
-    //No tendria que ser getMatches?
     @GET("matches")
     Call <List<MatchDataResponse>> getListMatches();
 
