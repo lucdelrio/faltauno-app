ffaa967772588d0fe7d6b54acd88fcfa8403e60b
lucdelrio <lucasgdelrio93@gmail.com>
2017-06-24 01:11:36 -0300

    Cambio botón mostrar jugadores

--- app/src/main/java/com/app/faltauno/activities/MostrarPartido.java
+++ app/src/main/java/com/app/faltauno/activities/MostrarPartido.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -41,6 +44,11 @@ public class MostrarPartido extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_mostrar_partido);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         Bundle index = intent.getExtras();
@@ -136,5 +144,24 @@ public class MostrarPartido extends AppCompatActivity {
             postularseButton.setVisibility(View.INVISIBLE);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mostrar_jugadores, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.jugadores) {
+            Intent jugadoresPartido = new Intent(MostrarPartido.this, JugadoresPostulados.class);
+            jugadoresPartido.putExtra("idPartido", this.idPartidoMostrado);
+            startActivity(jugadoresPartido);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
